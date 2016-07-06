e41315f8cf1def4c962a9ce2f35d098db758923d
Luke Maurer <maurerl@cs.uoregon.edu>
2016-07-07 00:23:28 +0100

    Fix logic for tossing context at join point call
    Firstly, we need to trim the context even if we're substituting rather than
    keeping the call intact, so completeCall is too late. Now it happens in
    simplIdF. Secondly, we can't trust the joinPointInfo on the occurrence; we need
    to look it up in the in-scope set.

--- compiler/simplCore/Simplify.hs
+++ compiler/simplCore/Simplify.hs
@@ -1502,9 +1502,9 @@ simplVar env var
 simplIdF :: SimplEnv -> InId -> SimplCont -> SimplM (SimplEnv, OutExpr)
 simplIdF env var cont
   = case substId env var of
-        DoneEx e             -> simplExprF (zapSubstEnv env) e cont
-        ContEx tvs cvs ids e -> simplExprF (setSubstEnv env tvs cvs ids) e cont
-        DoneId var1          -> completeCall env var1 cont
+        DoneEx e             -> simplExprF (zapSubstEnv env) e cont'
+        ContEx tvs cvs ids e -> simplExprF (setSubstEnv env tvs cvs ids) e cont'
+        DoneId var1          -> completeCall env var1 cont'
                 -- Note [zapSubstEnv]
                 -- The template is already simplified, so don't re-substitute.
                 -- This is VITAL.  Consider
@@ -1514,19 +1514,15 @@ simplIdF env var cont
                 -- We'll clone the inner \x, adding x->x' in the id_subst
                 -- Then when we inline y, we must *not* replace x by x' in
                 -- the inlined copy!!
-
----------------------------------------------------------
---      Dealing with a call site
-
-completeCall :: SimplEnv -> OutId -> SimplCont -> SimplM (SimplEnv, OutExpr)
-completeCall env var cont
-  | JoinPoint arity <- idJoinPointInfo var
-  = completeTrimmedCall env var (trim_cont arity cont)
-      -- Note [Case-of-case and join points]
-  | otherwise
-  = completeTrimmedCall env var cont
   where
-    -- Need to separate the invocation of the join point from the outer context
+    var_in_scope = refineFromInScope (getInScope env) var
+    cont' | JoinPoint arity <- idJoinPointInfo var_in_scope
+          = trim_cont arity cont
+          | otherwise
+          = cont
+    
+    -- Drop outer context from join point invocation
+    -- Note [Case-of-case and join points]
     trim_cont 0 cont@(Stop {})
       = cont
     trim_cont 0 cont
@@ -1538,8 +1534,11 @@ completeCall env var cont
     trim_cont _ cont
       = pprPanic "completeCall" $ ppr var $$ ppr cont
 
-completeTrimmedCall :: SimplEnv -> OutId -> SimplCont -> SimplM (SimplEnv, OutExpr)
-completeTrimmedCall env var cont
+---------------------------------------------------------
+--      Dealing with a call site
+
+completeCall :: SimplEnv -> OutId -> SimplCont -> SimplM (SimplEnv, OutExpr)
+completeCall env var cont
   = do  {   ------------- Try inlining ----------------
           dflags <- getDynFlags
         ; let  (lone_variable, arg_infos, call_cont) = contArgs cont
